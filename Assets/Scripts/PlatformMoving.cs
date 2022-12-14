b9e55cb9a1cb0c743a3bcd29e760771129e100c9
Teacher2905 <kirshvedov22@gmail.com>
2022-12-14 21:00:52 +0300

    14 12

--- Assets/Scripts/PlatformMoving.cs
+++ Assets/Scripts/PlatformMoving.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class PlatformMoving : MonoBehaviour
 {
-    [SerializeField] private Transform _start;
-    [SerializeField] private Transform _finish;
+    public Transform _start;
+    public Transform _finish;
     [SerializeField] private PlatformStorage _storage;
+    [SerializeField] private int _indexInStorage;
+    private Transform[] _platforms;
+    private bool _hit;
+
+    private void Start()
+    {
+        _platforms = _storage.Platforms;
+    }
 
     private void Update()
     {
-        Debug.DrawRay(_start.position, Vector3.up * 10);
-        Debug.DrawRay(_finish.position, Vector3.up * 10);
+        _hit = GetComponent<GroundRay>().hit;
+
+        if (_hit)
+        {
+            _platforms[_indexInStorage].transform.position =
+                 _platforms[_indexInStorage + 1].GetComponent<PlatformMoving>()._finish.position + transform.position;
+        }
     }
 }
